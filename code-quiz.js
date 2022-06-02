84ab0baf25c100683cb8393d912ff6802cdd4722
schadhuntsman <104479454+schadhuntsman@users.noreply.github.com>
2022-06-01 23:37:14 -0700

    did a lot of work

--- code-quiz.js
+++ code-quiz.js
@@ -1,13 +1,11 @@
-var btnChoiceEl = document.getElementById('button-choices')
-var quizContainerEl = document.getElementById('questions-container')
-var questionEl = document.getElementById('question')
-var quizOptions = document.getElementById('answer-options')
-var strtButton = document.getElementById('start-quiz');
-
-var nxtButton = document.getElementById('next-button')
+const strtButton = document.getElementById('start-quiz')
+const nxtButton = document.getElementById('next-question')
+const quizContainerEl = document.getElementById('questions-container')
+const questionEl = document.getElementById('question')
+const btnChoiceEl = document.getElementById('answer-options')
 
     //math.random is set to -.5 so that it's less than 0 or above 0 50% of the time
-let randomizer, displayedQuestions
+
 // const timer = document.createElement('div');
 // let time = 100;
 // timerEl.textContent = 0;
@@ -18,17 +16,22 @@ let randomizer, displayedQuestions
 //     countdownu(time);
 // }, 1000)
 // }
+let randomizer, displayedQuestions
+
 strtButton.addEventListener('click',strtGame)
 
-function strtGame() {
-    
-    displayedQuestions = 0;
-    
-    var randomizer = quizQuestions.sort(() => Math.random() - .5)
-    nxtButton.addEventListener('click',() => {
-    displayedQuestions++,
+nxtButton.addEventListener('click',() => {
+    displayedQuestions++
     nxtQuestion()
 })
+
+function strtGame() {    
+    strtButton.classList.add('hide')
+     randomizer = quizQuestions.sort(() => Math.random() - .5)   
+     displayedQuestions = 0;  
+     quizContainerEl.classList.remove('hide')
+     nxtQuestion()
+}
 
 //start at 0 because we're starting at the fist question
 
@@ -39,66 +42,64 @@ function nxtQuestion ()  {
 
 function displayQuestion(question) {
     questionEl.innerText = question.question
-    question.answers.array.forEach(answers => {
-     btnChoiceEl = document.createElement('shownAnswrButtons') 
-     shownAnswrButtons.innerText = answers.text
-     shownAnswrButtons.classList.add('btn')
-    if (answers.correct) {
-      shownAnswrButtons.dataset.correct = answer.correct
+    question.answers.forEach(answer => {
+    const button = document.createElement('button') 
+     button.innerText = answer.text
+     button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
  }
-    shownAnswrButtons.addEventListener('click', chooseAnswer)   
-    quizOptions.appendChild(shownAnswrButtons)
+    button.addEventListener('click', chooseAnswer)   
+    btnChoiceEl.appendChild(button)
               
 })
+}
 
 function quizReset() {   
+    clearStatus(document.body)
+    nxtButton.classList.add('hide')
     while (answerOptions.firstChild) {
-        answerOptions.removeChild
-        (answerOptions.firstChild)
+        answerOptions.removeChild(answerOptions.firstChild)      
     }        
 }
 
-    displayQuestion(randomizer[displayedQuestions])
+    // displayQuestion(randomizer[displayedQuestions])
     
-}
-
-function chooseAnswer()  {
-    questionEl.innerText = quizQuestions.quizQuestions
-//    
-}
 
 
 function chooseAnswer(e) {
-  const  chosenBtn = e.target
-  const correct = chosenBtn.dataset.correct
+  const  clickedTarget = e.target
+  const correct = clickedTarget.dataset.correct
   setStatusClass(document.body, correct)
-    Array.from(quizOptions.children).forEach(shownAnswrButtons => {
+    Array.from(btnChoiceEl.children).forEach(shownAnswrButtons => {
         setStatusClass(shownAnswrButtons, shownAnswrButtons.dataset.correct)
     })
-}
 if (randomizer.length > displayedQuestions + 1) {
     } else {
-        strtButton.innerText = 'Try Again'
+        strtButton.innerText = 'Try Again'    
     }
-
+}
 function setStatusClass(element, correct) {
-    clearDecision(element)
+    clearStatus(element)
     if (correct) {
         element.classList.add('You got it!')
-    } if (correct=false) {
+    } else {
         element.classList.add('Nope, sorry...')
     }
 } 
 
+function clearStatus(element) {
+    element.classList.remove('You got it!')
+    element.classList.remove('Nope, sorry...')
+}
 const quizQuestions = [
     {
-        question: 'What is over 9000?',
-        answers: [
-            {text: 'napa', correct: false },
-            {text: 'goku', correct: true },
-            {text: 'vegeta', correct: false},
-            {text: 'krillin lol', correct: false}
-        ]
-}
+      question: 'What is over 9000??',
+      answers: [
+        { text: '4', correct: true },
+        { text: '22', correct: false }
+      ]
+    }
 ]
-}
+
+
